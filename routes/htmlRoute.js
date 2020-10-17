0494c3555a2cc4c363fe6e6d176249c41163598b
cat - lin <plummorgan@gmail.com>
2020-10-17 13:14:55 -0700

    added routes

---
+++ routes/htmlRoute.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const router = require('express').Router();
+
+//Transfers the file at the given path. Sets the Content-Type response HTTP header field based on the filename’s extension.
+
+//route to the index.html
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
+
+//route to the notes.html
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/notes.html'));
+});
+
+module.exports = router;
